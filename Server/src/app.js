17d11246f44574992ac3fe2e18c237e33cc1c24d
modycheif <ankitpandey200154@gmail.com>
2024-05-27 16:13:09 +0530

    ..

--- Server/src/app.js
+++ Server/src/app.js
@@ -28,18 +28,18 @@ app.use('/api/v1/chat', CHATROUTER);
 app.use('/api/v1/message', MESSAGEROUTER);
 
 // -------------------deployment-------------------
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "..", "Client", "dist")));
+// const __dirname1 = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname1, "..", "Client", "dist")));
 
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname1, "..", "Client", "dist", "index.html"))
-    );
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running..");
-    });
-}
+//     app.get("*", (req, res) =>
+//         res.sendFile(path.resolve(__dirname1, "..", "Client", "dist", "index.html"))
+//     );
+// } else {
+//     app.get("/", (req, res) => {
+//         res.send("API is running..");
+//     });
+// }
 
 // -------------------deployment-------------------
 
