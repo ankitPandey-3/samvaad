0fb8656a46e961a24e386b5a6285f32c85358d3e
modycheif <ankitpandey200154@gmail.com>
2024-05-27 23:45:42 +0530

    ..

--- Server/src/app.js
+++ Server/src/app.js
@@ -13,16 +13,17 @@ const app = express();
 app.use(cors(
     {
         origin: process.env.CORS_ORIGIN,
-        credentials: true
+        headers: ["Content-Type"],
+        credentials: true,
     }
 ));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 app.get('/', (req, res)=> {
     res.send('API is running..')
