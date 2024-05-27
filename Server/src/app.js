fa12d310b09b5bc4d97532e6102a6f5d2f173b85
modycheif <ankitpandey200154@gmail.com>
2024-05-27 17:12:13 +0530

    ..

--- Server/src/app.js
+++ Server/src/app.js
@@ -18,7 +18,7 @@ app.use(cors(
 ));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
