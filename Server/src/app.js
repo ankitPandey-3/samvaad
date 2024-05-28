86923424d4f723a1e78b2e9fd4e42867ec9cc2b3
modycheif <ankitpandey200154@gmail.com>
2024-05-28 16:51:23 +0530

    ..

--- Server/src/app.js
+++ Server/src/app.js
@@ -18,13 +18,15 @@ const app = express();
 //     }
 // ));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     next();
+// });
+
+app.use(cors());
 
 app.get('/', (req, res)=> {
     res.send('API is running..')
