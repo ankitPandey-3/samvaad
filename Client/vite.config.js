ad202dabdd656550563209bced30a8a0d61003ad
modycheif <ankitpandey200154@gmail.com>
2024-05-28 17:00:11 +0530

    ..

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { createProxy } from 'http-proxy-middleware'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -7,12 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: createProxy({
-      '/api': {
-        target: 'https://video-chat-application-mern.onrender.com', // Replace with your backend URL
-        changeOrigin: true,
-        secure: false, // Set to false if your backend has self-signed SSL certificates
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
+      '/api': 'https://video-chat-application-mern.onrender.com'
     })
   }
 
