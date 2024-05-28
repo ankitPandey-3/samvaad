f0e8b7fea1b73d91137183e920f107079606bb30
modycheif <ankitpandey200154@gmail.com>
2024-05-28 16:34:38 +0530

    ..

--- Server/src/app.js
+++ Server/src/app.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://samvaad-chi.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
 
