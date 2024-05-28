1ff48a3694816c862e315cb623f61b28b9190092
modycheif <ankitpandey200154@gmail.com>
2024-05-28 17:09:29 +0530

    ..

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  // axios.defaults.baseURL = "https://video-chat-application-mern.onrender.com";
+  axios.defaults.baseURL = "https://video-chat-application-mern.onrender.com";
   axios.defaults.withCredentials = true;
 
   return (
