20d0090637978f627e0e5e3222655ea8a1fcdd7c
modycheif <ankitpandey200154@gmail.com>
2024-05-31 13:45:22 +0530

    sameSite

--- Server/src/controllers/user.controllers.js
+++ Server/src/controllers/user.controllers.js
@@ -96,6 +96,7 @@ const userSignin = async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
   };
 
   return res
--- Server/src/middlewares/auth.middleware.js
+++ Server/src/middlewares/auth.middleware.js
@@ -3,12 +3,12 @@ import jwt from 'jsonwebtoken';
 
 export const checkAuth = async(req, res, next)=>{
     const tokens = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    // console.log(tokens,"==========")
     if(!tokens){
         return next();
     }
     const decodedToken = jwt.verify(tokens, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
-    console.log(user)
     if(!user){
         return next();
     }
